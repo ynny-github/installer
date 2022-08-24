6769705ca765ca6947be87fe8ee86c29f006597e
Yuya Nagai <open@ynny.dev>
2022-08-24 23:19:14 +0900

    VSCodeから自動インストールするときに呼び出すinstallerを追加

---
+++ installer_for_vscode.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+exec_cmd_with_sudo () {
+    if [ "${USER}" = "root" ]; then
+        $@
+    else
+        sudo $@
+    fi
+}
+
+do_exist_cmd () {
+    if type $1 &> /dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+if [ -f /.dockerenv ]; then
+    if do_exist_cmd "apt"; then
+        exec_cmd_with_sudo apt update
+        exec_cmd_with_sudo apt install -y fish
+    elif do_exist_cmd "yum"; then
+        exec_cmd_with_sudo yum update
+        exec_cmd_with_sudo yum install -y fish
+    fi
+fi
+
+if [ $(uname -s) = "Darwin"]; then
+    brew install chezmoi
+    chezmoi init --apply https://github.com/ynny-github/dotfiles.git
+else
+    cd ~/installer
+    chmod +x chezmoi_installer
+    ./chezmoi_installer.sh -- init --apply https://github.com/ynny-github/dotfiles.git
+
+fi
+
+rm -rf ~/installer
